2942082ebd77457dc1b1be81b8f895a8bbdfde86
JethroWang <smilewang997700@gmail.com>
2025-10-02 10:48:51 +0800

    1002_1

---
+++ lesson5/AGENTS.md
@@ -0,0 +1,3 @@
+## 這是一個flask的專案
+## 專案使用uv 虛擬環境,如果要執行'python app.py', 請改為使用'uv run .\lesson5\python app.py'
+## Flask 網頁樣板使用預設的templates資料夾, CSS, js, image使用預設的static資料夾
--- lesson5/app.py
+++ lesson5/app.py
@@ -2,21 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
-"""
-@app.route("/")
-這個裝飾器定義了 Flask 應用程序的根 URL ("/")。
-當使用根 URL 访问應用程序時，應該調用 `index` 函數。
 
-Returns:
-    str: HTML 字符串 "<h1>Hello, World!</h1>"
-"""
 @app.route("/") #下面要立即定義一個function
 def index():
     """根節點，呈現 index.html"""
     return render_template("index.html")
 
 def main():
-    """啟動應用程式（教學用：啟用 debug 模式）"""
     # 在開發環境下使用 debug=True，部署時請關閉 (開放給所有人正式的瀏覽器host="0.0.0.0" 開發者模式自己用host="127.0.0.1")
     app.run(host="127.0.0.1", port=5000, debug=True)
 
