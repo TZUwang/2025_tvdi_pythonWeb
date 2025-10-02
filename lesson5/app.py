785cbf04479e631f994ddd05347b3367e107e263
JethroWang <smilewang997700@gmail.com>
2025-10-02 16:39:58 +0800

    1002_7

--- lesson5/app.py
+++ lesson5/app.py
@@ -8,9 +8,15 @@ def index():
     """根節點，呈現 index.html"""
     return render_template("index.html")
 
+@app.route("/machine")
+def machine():
+    return render_template("machine.html")
+
+
+
 def main():
     # 在開發環境下使用 debug=True，部署時請關閉 (開放給所有人正式的瀏覽器host="0.0.0.0" 開發者模式自己用host="127.0.0.1")
     app.run(host="127.0.0.1", port=5000, debug=True)
 
 if __name__ == "__main__":
-    app.run()
+    main()
